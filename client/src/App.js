523d9ffa34876e9ab7e24a721948e95408ed063a
Cameron Wright <arabia.cw@gmail.com>
2019-07-26 04:30:31 -0400

    react routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
 import './App.css';
+import RequestJob from "./pages/RequestJob";
 import SideNav from "./components/SideNav.";
+
+
+
+
 
 
 
@@ -8,14 +14,20 @@ import SideNav from "./components/SideNav.";
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <div>
       <SideNav/>
-    </div>
-    
-
-  );
-
   
+      <Switch>
+        <Route path="/RequestJob" component={RequestJob} />
+        <Route path="/AvailableJob" component={RequestJob} />
+        <Route path="/Ratings" component={RequestJob} />
+      </Switch>
+      </div>
+    </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/RequestJob.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+
+
+
+class RequestJob extends Component {
+    
+  render() {
+    return (
+      <div>
+          <h1>My Name is Cameron</h1>
+      </div>
+    );
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default RequestJob;
